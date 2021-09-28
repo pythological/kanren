4a1edb8de1b8a19320a5c71143a23b22f1ea6521
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2021-09-28 00:24:07 -0500

    Fix accounts example

--- examples/user_classes.py
+++ examples/user_classes.py
@@ -1,10 +1,9 @@
 from operator import add, gt, sub
 
-from account import Account
-
-from kanren import membero, run, unifiable, var
+from examples.account import Account
+from kanren import eq, membero, run, unifiable, var
 from kanren.core import lall
-from kanren.term import term  # noqa: F401
+from kanren.term import applyo, term  # noqa: F401
 
 unifiable(Account)  # Register Account class
 
@@ -15,11 +14,11 @@ accounts = (
 )
 
 # optional name strings are helpful for debugging
-first = var("first")
-last = var("last")
-ident = var("ident")
-balance = var("balance")
-newbalance = var("newbalance")
+first = var(prefix="first")
+last = var(prefix="last")
+ident = var(prefix="ident")
+balance = var(prefix="balance")
+newbalance = var(prefix="newbalance")
 
 # Describe a couple of transformations on accounts
 source = Account(first, last, ident, balance)
@@ -30,12 +29,16 @@ theorists = ("Adam", "Carl")
 theorist_bonus = lall(
     membero(source, accounts),
     membero(first, theorists),
-    add(10, balance, newbalance),
+    applyo(add, (10, balance), newbalance),
 )
 
 # Take $10 from anyone with more than $100
+a = var(prefix="a")
 tax_the_rich = lall(
-    membero(source, accounts), gt(balance, 100), sub(balance, 10, newbalance)
+    membero(source, accounts),
+    applyo(gt, (balance, 100), a),
+    eq(a, True),
+    applyo(sub, (balance, 10), newbalance),
 )
 
 print("Take $10 from anyone with more than $100")
